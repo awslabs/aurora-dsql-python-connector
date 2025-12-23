14ef07db4a76538479b0143a03f7094b71f5eee7
Aleksandar Maksimovic <amaksimovic3@hotmail.com>
2025-12-23 07:55:28 -0800

    Standardize folder structure and format (#20)
    Restructures examples with example_preferred as the golden path
    (pool + connector).
    
    - Renamed files to follow naming convention
    - Organized into alternatives/ folder structure
    - Added README in alternatives/ folder
    - Added tests for all examples
    
    Co-authored-by: Aleksandar Maksimovic <alemaksi@amazon.com>

---
+++ examples/psycopg/test/alternatives/pool/test_example_with_async_connection_pool.py
@@ -0,0 +1,19 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+"""
+
+import pytest
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../../src/alternatives/pool'))
+from example_with_async_connection_pool import main
+
+
+# Smoke tests that our async example works fine
+@pytest.mark.asyncio
+async def test_example_with_async_connection_pool():
+    try:
+        await main()
+    except Exception as e:
+        pytest.fail(f"Unexpected exception: {e}")
--- examples/psycopg2/test/alternatives/no_connection_pool/test_example_with_no_connection_pool.py
+++ examples/psycopg2/test/alternatives/no_connection_pool/test_example_with_no_connection_pool.py
@@ -3,12 +3,16 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 """
 
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../../src/alternatives/no_connection_pool'))
+
 import pytest
-from example import main
+from example_with_no_connection_pool import main
 
 
 # Smoke tests that our example works fine
-def test_example():
+def test_example_with_no_connection_pool():
     try:
         main()
     except Exception as e:
