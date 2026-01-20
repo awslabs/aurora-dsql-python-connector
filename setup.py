890207f512fc312f14f1dcb70e822304ac8db7ad
Daniel Frankcom <frankcom@amazon.com>
2026-01-20 15:56:24 -0800

    Use # format syntax for header comments

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """Setup script for Aurora DSQL connector."""
 
 from setuptools import setup
