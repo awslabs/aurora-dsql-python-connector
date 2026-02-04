eb365e24f8ca0fbeef1f1c2eecdd9605e39918b0
Aleksandar Maksimovic <amaksimovic3@hotmail.com>
2026-02-04 10:39:19 -0800

    Deprecate repository - moved to aurora-dsql-connectors monorepo
    This repository has been consolidated into the Aurora DSQL Connectors
    monorepo at https://github.com/awslabs/aurora-dsql-connectors
    
    The Python connector is now located at:
    https://github.com/awslabs/aurora-dsql-connectors/tree/main/python/connector
    
    The PyPI package aurora-dsql-python-connector continues to work and is
    now published from the new monorepo.

--- src/aurora_dsql_asyncpg/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-from dsql_core._version import __version__
-
-from .connector import connect
-from .pool import create_pool
-
-__all__ = ["connect", "create_pool", "__version__"]
